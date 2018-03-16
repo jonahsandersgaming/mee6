8e76e33397a8c25e45da84cbbad0ba16ff5655fe
laggycomputer <37320033+laggycomputer@users.noreply.github.com>
2018-03-16 16:00:12 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/sh 
 echo Starting installation...
 apt-get update  # To get the latest package lists 
+apt-get install git
+apt-get install python3
+apt-get install python3-pip
 apt-get install redis-server -y 
+cd ~/
+git clone https://github.com/jtagt/mee6
+python3 -m pip install -U https://github.com/rapptz/discord.py/archive/async.zip#egg=discord.py[voice]
 cd mee6/chat-bot 
 pip3 install -r requirements.txt 
 cd ~ 
