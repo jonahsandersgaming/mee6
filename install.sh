15cfef47086042dfbf1fda5a519fb3fae66d5491
Jackson <35349831+jtagt@users.noreply.github.com>
2018-03-16 18:45:10 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,13 @@
-new shell script
+#!/bin/sh
+echo starting installing progress
+apt-get update  # To get the latest package lists
+apt-get install redis-server -y
+cd mee6/chat-bot
+pip3 install -r requirements.txt
+cd ~
+cd mee6/website
+pip3 install -r requirements.txt
+cd ~
+cd mee6/
+echo done installing.
+#etc.
